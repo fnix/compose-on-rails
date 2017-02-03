4a2f9e63bb2953657cc137bd04c112fae9f3ab78
Kadu Diógenes <kadu@fnix.com.br>
2017-02-03 20:38:45 -0200

    Remove version control after bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # --force overrides README.md, .gitignore and Gemfile
+rm -rf .git
+rm .gitignore
 docker-compose run web rails new . --force --database=postgresql --skip-bundle
 sudo chown -R $USER:$USER .
 mv spring_client.rb config/

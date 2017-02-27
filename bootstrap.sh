1fad0630845346c406bde9cab7823521826db908
Kadu Diógenes <kadu@fnix.com.br>
2017-02-27 17:06:07 -0300

    Improving bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 # --force overrides README.md, .gitignore and Gemfile
 rm -rf .git
 rm .gitignore
+docker-compose build
 docker-compose run web rails new . --force --database=postgresql --skip-bundle
 mv spring_client.rb config/
 mv database.yml config/

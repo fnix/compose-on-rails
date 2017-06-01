e0bc99d27d316e8ed65472b41695ece87d37ca34
Kadu Diógenes <kadu@fnix.com.br>
2017-06-01 12:40:56 -0300

    Skip minitest

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# --force overrides README.md, .gitignore and Gemfile
 rm -rf .git
 rm .gitignore
 docker-compose build
-docker-compose run web rails new . --force --database=postgresql --skip-bundle
+# --force overrides README.md, .gitignore and Gemfile
+docker-compose run web rails new . --force --database=postgresql --skip-bundle --skip-test-unit
 mv spring_client.rb config/
 mv database.yml config/
 docker-compose run web bundle
